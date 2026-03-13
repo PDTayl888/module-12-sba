2acdb0ab8ddf14a993ccc428c89b7474889853d5
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-03-13 18:22:09 -0400

    started server.js

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 require('dotenv')
 
 const express = require('express');
+const movieRoutes = require('./routes/movieRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use('/api', movieRoutes);
+
+app.listen(PORT, () => {
+    console.log(`server running and listening at port ${PORT}`);
+})
