c0fb531750fb4981388e46d908f78e62d2276012
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-03-13 22:07:51 -0400

    edited monvieController

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -4,5 +4,18 @@ const searchMovies = async (requestAnimationFrame, res) => {
     try {
         const title = req.query.title;
 
-        if (!title)
+        if (!title) {
+            return res.status(400).json({ error: "error message"});
+        }
+
+        const response = await axios.get('http://www.omdbapi.com/', {
+            params: {
+                s: title,
+                apikey: process.env.OMDB_API_KEY
+            }
+        });
+
+        res.json(response.data);
+    } catch (error) {
+        console.log()
     }
