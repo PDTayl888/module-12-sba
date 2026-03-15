c04d6b41bcd2b4f0543d47312636900fe918540b
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-03-15 00:02:26 -0400

    added routes

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -2,4 +2,7 @@ const express = require('express');
 const router = express.Router();
 const movieController = require('../controllers/movieController');
 
-router.get()
+router.get('/search', movieController.searchMovies);
+router.get('/movies/:id', movieController.getMovieDetails);
+
+module.exports = router;
