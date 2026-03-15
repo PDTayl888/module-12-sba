7a479e0aa8c0553a0067367bcecbc56c2f37c8da
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-03-14 23:53:02 -0400

    edited movie controller

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,34 +1,42 @@
-const axios = require('axios');
+const axios = require("axios");
 
 const searchMovies = async (requestAnimationFrame, res) => {
-    try {
-        const title = req.query.title;
+  try {
+    const title = req.query.title;
 
-        if (!title) {
-            return res.status(400).json({ error: "error message"});
-        }
+    if (!title) {
+      return res.status(400).json({ error: "error message" });
+    }
 
-        const response = await axios.get('http://www.omdbapi.com/', {
-            params: {
-                s: title,
-                apikey: process.env.OMDB_API_KEY
-            }
-        });
+    const response = await axios.get("http://www.omdbapi.com/", {
+      params: {
+        s: title,
+        apikey: process.env.OMDB_API_KEY,
+      },
+    });
 
-        res.json(response.data);
-    } catch (error) {
-        console.log()
-    };
+    res.json(response.data);
+  } catch (error) {
+    console.log();
+  }
+};
 
-    const getMovieDetails = async (req, res) => {
-        try {
+const getMovieDetails = async (req, res) => {
+  try {
+    const imbId = req.id.params.id;
+    const response = await axios.get("http://www.omdbapi.com/", {
+      params: {
+        i: imdbId,
+        apikey: process.env.OMDB_API_KEY,
+      },
+    });
+    req.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "failed to fetch movie details from OMDB" });
+  }
+};
 
-        } catch (error) {
-
-        }
-    };
-
-    module.exports = {
-        searchMovies,
-        getMovieDetails
-    };
+module.exports = {
+  searchMovies,
+  getMovieDetails,
+};
